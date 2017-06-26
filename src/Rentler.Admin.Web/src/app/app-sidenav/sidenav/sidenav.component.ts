ebf998fe23f7a4724d7a8e05eec7d95307893ed4
kyam <kyamc@zylun.com>
2017-06-27 04:08:08 +0800

    UI Fixes and Page Addition

--- src/Rentler.Admin.Web/src/app/app-sidenav/sidenav/sidenav.component.ts
+++ src/Rentler.Admin.Web/src/app/app-sidenav/sidenav/sidenav.component.ts
@@ -15,31 +15,35 @@ import {
 export class SidenavComponent implements OnDestroy {
 
     private _subscription;
-    public isOpen = true;
+    public isOpen;
     public mediaChange;
     public mode: string;
     constructor(public media: ObservableMedia) {
+    
         console.log("this.mode", this.mode);
-        this.mode = "side";
+        console.log("Is Open", this.isOpen);
+        console.log("change", this.mediaChange);
+        console.log("media", media);
+     
         this._subscription = media.subscribe((change: MediaChange) => {
             this.isOpen = (change.mqAlias !== 'xs');
             this.mediaChange = change;
-            
-            (change.mqAlias != 'xs') ? this.mode = "side" : this.mode = "over";
-     
+            console.log("is open when refresh", this.isOpen);
+            console.log("Subscription", this._subscription);
+            (change.mqAlias !== 'xs') ? this.mode = "side" : this.mode = "over";
 
-        })
+
+        });
 
 
     }
 
     toggleSideNav() {
 
+        console.log("watch mode",this.isOpen);
         this.isOpen = !this.isOpen;
         this.mode = "over";
-        console.log("is open", this.isOpen);
-
     }
-    ngOnDestroy() { this._subscription.unsubscribe(); console.log("Destroy"); }
+    ngOnDestroy() { console.log("TEST")/*this._subscription.unsubscribe(); console.log("Destroy");*/ }
 
 }
---
+++ src/Rentler.Admin.Web/src/app/component-viewer/idverify/idverify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-idverify',
+  templateUrl: './idverify.component.html',
+  styleUrls: ['./idverify.component.css']
+})
+export class IdverifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
