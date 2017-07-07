335ad297dfe3904e1ef7f539c7c41b463bc10211
kyamc <kyamc@zylun.com>
2017-07-07 15:17:13 +0800

    Added User Service api connection

---
+++ src/Rentler.Admin.Web/src/app/Services/users.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+
+@Injectable()
+export class UsersService {
+    constructor(private _http: Http) {
+        
+    }
+
+    extractData(res: Response)
+    {
+
+    }
+
+    GetAll()
+    {
+        var a = new Headers();
+        return this._http.get("/api/GetUsers").map(this.extractData);
+    }
+
+}
