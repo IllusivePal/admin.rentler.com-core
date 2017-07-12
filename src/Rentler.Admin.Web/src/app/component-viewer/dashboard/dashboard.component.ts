e397c340d895c57e50c48a0679d5523194ac7560
kyamc <kyamc@zylun.com>
2017-07-12 16:58:47 +0800

    Added MdDatatable,table module and cdktable

--- src/Rentler.Admin.Web/src/app/component-viewer/dashboard/dashboard.component.ts
+++ src/Rentler.Admin.Web/src/app/component-viewer/dashboard/dashboard.component.ts
@@ -1,6 +1,14 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit,ViewChild } from '@angular/core';
 import { UsersService } from '../../Services/users.service';
 import { ProgressbarService } from '../../Services/progressbar.service';
+
+import { DataSource } from '@angular/cdk';
+import { MdPaginator } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,15 +17,19 @@ import { ProgressbarService } from '../../Services/progressbar.service';
   providers: [UsersService]
 })
 export class DashboardComponent implements OnInit {
+    displayedColumns = ['userId', 'userName', 'progress', 'color'];
+    exampleDatabase = new ExampleDatabase();
+    dataSource: ExampleDataSource | null;
 
+    @ViewChild(MdPaginator) paginator: MdPaginator;
     constructor(private _userService: UsersService,
         private _progressBar: ProgressbarService) { }
 
     ngOnInit() {
-
-        this._userService.GetAll().subscribe((res) => {
-            console.log("THIS IS AWESOME SUBSCRIBE", res);
-        });
+        this.dataSource = new ExampleDataSource(this.exampleDatabase, this.paginator);
+        //this._userService.GetAll().subscribe((res) => {
+           // console.log("THIS IS AWESOME SUBSCRIBE", res);
+        //});
     }
 
     startProgress()
@@ -26,4 +38,78 @@ export class DashboardComponent implements OnInit {
         this._progressBar.start("indeterminate","primary");
     }
 
+}
+
+/** Constants used to fill up our data base. */
+const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+    'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+    'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+    'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+
+export interface UserData {
+    id: string;
+    name: string;
+    progress: string;
+    color: string;
+}
+
+
+export class ExampleDatabase {
+    dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
+    get data(): UserData[] { return this.dataChange.value; }
+    constructor() {
+        // Fill up the database with 100 users.
+        for (let i = 0; i < 100; i++) { this.addUser(); }
+    }
+
+    /** Adds a new user to the database. */
+    addUser() {
+        const copiedData = this.data.slice();
+        copiedData.push(this.createNewUser());
+        this.dataChange.next(copiedData);
+    }
+    /** Builds and returns a new User. */
+    private createNewUser() {
+        const name =
+            NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+            NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+        return {
+            id: (this.data.length + 1).toString(),
+            name: name,
+            progress: Math.round(Math.random() * 100).toString(),
+            color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+        };
+    }
+}
+/**
+ * Data source to provide what data should be rendered in the table. Note that the data source
+ * can retrieve its data in any way. In this case, the data source is provided a reference
+ * to a common data base, ExampleDatabase. It is not the data source's responsibility to manage
+ * the underlying data. Instead, it only needs to take the data and send the table exactly what
+ * should be rendered.
+ */
+export class ExampleDataSource extends DataSource<any> {
+  constructor(private _exampleDatabase: ExampleDatabase, private _paginator: MdPaginator) {
+    super();
+  }
+
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<UserData[]> {
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._paginator.page,
+    ];
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      const data = this._exampleDatabase.data.slice();
+
+      // Grab the page's slice of data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return data.splice(startIndex, this._paginator.pageSize);
+    });
+  }
+
+  disconnect() {}
 }
